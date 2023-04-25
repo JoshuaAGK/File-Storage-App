95ec90d4dca0833d26def0df07651f7559935b3e
Josh <JoshuaAGKelley@gmail.com>
2023-04-25 22:44:54 +0100

    Save file UUID & path to MongoDB

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,16 +3,32 @@ import { Router } from 'express';
 const router: Router = express.Router();
 const multer = require('multer');
 import writeFile from '../services/writeFile';
+import { client } from "../config/mongodb";
 
 router.post("/uploadfile", multer().any(), async (req: any, res: any) => {
-    console.log(req.body);
-    console.log(req.files);
-    res.send("ok");
     const fileName = req.files[0].originalname;
     const fileBuffer = req.files[0].buffer;
-    const teamName = req.body.team;
+    const teamName = req.body.teamName;
+    const uid = req.body.uid;
     const path = `filestore/${teamName}/`;
-    await writeFile(path, fileName, fileBuffer);
+    const response = await writeFile(path, fileName, fileBuffer);
+
+    // If writeFile failed, response will not be UUID. UUIDs are 36 characters long.
+    if (response.length !== 36) {
+        res.send(response);
+        return response;
+    }
+
+    const filesCollection = client.db(teamName).collection("files");
+    const insertResult = await filesCollection.insertOne({
+        path: path,
+        fileName: fileName,
+        uuid: response,
+        uploadedOn: new Date().toISOString(),
+        uploader: uid
+    });
+
+    res.send("ok");
 })
 
 export default router;
--- src/services/writeFile.ts
+++ src/services/writeFile.ts
@@ -1,14 +1,15 @@
 const fs = require('fs');
+import { v4 as uuidv4 } from 'uuid';
 
 function writeFile(path: string, fileName: string, content: any) {
     if (!fs.existsSync(path)){
         fs.mkdirSync(path, { recursive: true });
     }
     
-    return new Promise<string | void>((resolve, reject) => {
+    return new Promise<string>((resolve, reject) => {
         fs.writeFile(path + fileName, content, (err: string) => {
             if (!err) {
-                resolve();
+                resolve(uuidv4());
             }
             reject(err);
         })
