64479cf3c332fc369592203b890491cc8010014a
Josh <JoshuaAGKelley@gmail.com>
2023-04-30 01:46:54 +0100

    Get passhash of file

---
+++ src/services/filePasshash.ts
@@ -0,0 +1,77 @@
+import { client } from "../config/mongodb";
+import readFile from '../services/readfile';
+import HTTPResponse from "../interfaces/httpResponse";
+const jwt = require('jsonwebtoken');
+import { ObjectId } from "mongodb";
+import User from "../classes/user";
+import File from "../classes/file";
+
+async function filePasshash(fileID: string, token: string, passhash?: string): Promise<HTTPResponse> {
+    let file = new File;
+    let user = new User;
+
+    // Get user details from JWT
+    jwt.verify(token, process.env.TOKEN_SECRET, (err: any, verifiedJwt: any) => {
+        if(!err) {
+            user.teamName = verifiedJwt.teamName;
+            user._id = verifiedJwt.uid;
+        } else {
+            return {
+                responseCode: 401,
+                data: false
+            }
+        }
+    })
+
+    const filesCollection = await client.db(user.teamName).collection("files");
+    const foundFile = await filesCollection.findOne({ _id: fileID });
+    if (foundFile) {
+        file = foundFile;
+    }
+
+    // Check file was found
+    if (!file) {
+        return {
+            responseCode: 404,
+            data: false
+        }
+    }
+
+    // Check if logged-in user is authorised
+    const usersCollection = await client.db(user.teamName).collection("users");
+    const foundUser = await usersCollection.findOne({ _id: new ObjectId(String(user._id)) });
+
+
+    if (typeof passhash === "undefined") {
+        if (foundUser.permissions.read) {
+            return {
+                responseCode: 200,
+                data: file.passhash ?? null
+            }
+        } else {
+            return {
+                responseCode: 403,
+                data: false
+            }
+        }
+
+    } else {
+        if (foundUser.permissions.write) {
+            const setData = passhash ? { $set: { ["passhash"]: passhash } } : { $unset: { ["passhash"]: null } };
+            await filesCollection.updateOne({ _id: fileID }, setData)
+
+            return {
+                responseCode: 200,
+                data: true
+            }
+        } else {
+            return {
+                responseCode: 403,
+                data: false
+            }
+        }
+    }
+}
+
+
+export default filePasshash;
